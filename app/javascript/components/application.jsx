d256fcca3b27e0c2b446fb35391ba015c1cc05ba
Jay Chouhan <jcjay1235@gmail.com>
2022-06-11 02:21:20 +0530

    Setup react router

--- app/javascript/components/application.jsx
+++ app/javascript/components/application.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("root");
-  ReactDom.render(<h1>Hello</h1>, rootEl);
+  ReactDom.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+    rootEl
+  );
 });
